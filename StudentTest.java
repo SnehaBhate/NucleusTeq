aceb2900bc8b512b438b48686973d7cf1e986283
Sneha <snehabhate002@gmail.com>
2023-03-05 21:18:23 +0530

    oops task

---
+++ StudentTest.java
@@ -0,0 +1,34 @@
+/*Write a class Student having following properties and methods:
+Property: rollNumber, name, course, marks1, marks2, marks3
+Methods: calculateTotal(), calculateAverage(), calculateGrade()
+Create getter and setter methods.
+Create class StudentTest with main method for calling Student class.
+StudentTest should contain atleast 5 student objects with different grades.
+Implement toString method to display details of each student.
+Also display grades of each student.
+Store the student file in a package student;*/
+
+class Student {
+
+    int roll_no;
+    String name;
+    String course;
+    int marks1, marks2, marks3;
+
+    public void calculateTotal() {
+    }
+
+    public void calculateAverage() {
+    }
+
+    public void calculateGrade() {
+    }
+
+}
+
+public class StudentTest {
+
+public static void main(String args[])
+
+
+}
