18527f6b692e18b3119ceec1e3daca2c475622f0
Sneha <snehabhate002@gmail.com>
2023-03-14 17:39:14 +0530

    other oops tasks added

--- Student.java
+++ Student.java
@@ -1,5 +1,5 @@
 
-package student;
+/*package student;
 
 public class Student {
     private int roll_no;
@@ -25,6 +25,79 @@ public class Student {
     public void setName(String name, int roll_no) {
         this.name = name;
         this.roll_no = roll_no;
+    }
+
+}*/
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package student;
+
+public class Student {
+    private int roll_no, marks1, marks2, marks3;
+    private String name, course;
+    int total, avg;
+    String grade;
+
+    public void setter(int roll_no, int marks1, int marks2, int marks3, String name, String course) {
+        this.roll_no = roll_no;
+        this.marks1 = marks1;
+        this.marks2 = marks2;
+        this.marks3 = marks3;
+        this.name = name;
+        this.course = course;
+    }
+
+    public int getRoll_no() {
+        return roll_no;
+    }
+
+    public int getMarks1() {
+        return marks1;
+    }
+
+    public int getMarks2() {
+        return marks2;
+    }
+
+    public int getMarks3() {
+        return marks3;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    public void CalculateTotal() {
+        total = marks1 + marks2 + marks3;
+    }
+
+    public void CalculateAverage() {
+        avg = total / 3;
+    }
+
+    public void CalculateGrade() {
+        if (avg >= 80 && avg <= 100) {
+            grade = "A";
+        }
+        if (avg >= 70 && avg < 80) {
+            grade = "B";
+        } else if (avg >= 60 && avg < 70) {
+            grade = "C";
+        } else if (avg >= 50 && avg < 60) {
+            grade = "D";
+        }
+    }
+
+    public String toString() {
+        return "name=" + name + " course=" + course + " avg=" + avg + " total=" + total + " grade=" + grade;
     }
 
 }
--- StudentTest.java
+++ StudentTest.java
@@ -7,7 +7,7 @@ Create class StudentTest with main method for calling Student class.
 StudentTest should contain atleast 5 student objects with different grades.
 Implement toString method to display details of each student.
 Also display grades of each student.
-Store the student file in a package student;*/
+Store the student file in a package student;
 import student.Student;
 
 public class StudentTest {
@@ -16,4 +16,47 @@ public class StudentTest {
 
     }
 
+}*/
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package student;
+
+import student.*;
+
+public class StudentTest {
+    public static void main(String[] args) {
+
+        Student S1 = new Student();
+        S1.setter(101, 70, 68, 75, "Sneha", "MCA");
+        S1.CalculateTotal();
+        S1.CalculateAverage();
+        S1.CalculateGrade();
+
+        System.out.println(S1);
+
+        S1.setter(102, 80, 77, 67, "Aayushi", "MCA");
+        S1.CalculateTotal();
+        S1.CalculateAverage();
+        S1.CalculateGrade();
+
+        System.out.println(S1);
+
+        S1.setter(103, 80, 86, 87, "Khushi", "MBA");
+        S1.CalculateTotal();
+        S1.CalculateAverage();
+        S1.CalculateGrade();
+
+        System.out.println(S1);
+
+        S1.setter(104, 70, 76, 87, "Kanika", "MCA");
+        S1.CalculateTotal();
+        S1.CalculateAverage();
+        S1.CalculateGrade();
+
+        System.out.println(S1);
+    }
 }
